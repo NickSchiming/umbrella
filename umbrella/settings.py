bb663fa04a51d102ebad41540fb92061bc127fbd
NickSchiming <nickschiming@gmail.com>
2022-09-29 16:11:05 -0300

    modelo custom de autenticação

--- umbrella/settings.py
+++ umbrella/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'users.apps.UsersConfig',
+    "phonenumber_field",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -122,3 +123,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+PHONENUMBER_DEFAULT_REGION = 'BR'
+
+AUTHENTICATION_BACKENDS = ('django.contrib.auth.backends.ModelBackend',)
+
+AUTH_USER_MODEL = 'users.Usuario'
+
+
--- users/forms.py
+++ users/forms.py
@@ -1,12 +1,23 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UsernameField
 from django.contrib.auth import password_validation
+from django.forms import CharField, DateField
+from phonenumber_field.formfields import PhoneNumberField
+
+from . import models
 
 
 class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField(label="E-mail:")
+    username = UsernameField(
+        label='E-mail',
+        widget=forms.TextInput(attrs={'autofocus': True})
+    )
     nome = forms.CharField(label='Nome completo:', max_length=100)
+    cpf = forms.CharField(max_length=100)
+    telefone = PhoneNumberField()
+    endereco = CharField(max_length=150)
+    datanasc = DateField()
     password1 = forms.CharField(
         label="Senha:",
         strip=False,
@@ -15,5 +26,12 @@ class UserRegisterForm(UserCreationForm):
     )
 
     class Meta:
-        model = User
-        fields = ['nome', 'email', 'password1', 'password2']
+        model = models.Usuario
+        fields = ['username', 'nome', 'cpf', 'telefone', 'endereco',
+                  'datanasc', 'password1', 'password2']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['telefone'].widget.attrs.update({'class': 'mask-telefone'})
+        self.fields['datanasc'].widget.attrs.update({'class': 'mask-datanasc'})
+        self.fields['cpf'].widget.attrs.update({'class': 'mask-cpf'})
