813619da221cc975cc945d38355109ab1004efee
NickSchiming <nickschiming@gmail.com>
2022-12-04 17:24:05 -0300

    mexido no dir da database

--- umbrella/settings.py
+++ umbrella/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'umbrella.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
