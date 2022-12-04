de4ea0f7f8b9f1a7e088f5b6f4bf1c16b622d845
NickSchiming <nickschiming@gmail.com>
2022-12-04 17:05:11 -0300

    settings mudados

--- umbrella/settings.py
+++ umbrella/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-n*c@g-sy#l7l$!*5i%ge(^fbr@dqgu@-6c^^gttva^j7-^l^&3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['umbrella-tcc.herokuapp.com']
 
 
 # Application definition
